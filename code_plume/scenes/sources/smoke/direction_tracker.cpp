0992a21021d4f4c7185517b90bc3f9245e3b37c8
Vaughn <44435027+blazev01@users.noreply.github.com>
2025-05-21 09:47:22 +0800

    direction tracker GUI

--- code_plume/scenes/sources/smoke/direction_tracker.cpp
+++ code_plume/scenes/sources/smoke/direction_tracker.cpp
@@ -1,10 +1,12 @@
 #include "direction_tracker.hpp"
+#include "direction_tracker.hpp"
+#include "scenes/sources/smoke/smokeLayer.hpp"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-void direction_tracker::loadData(std::string filePath)
+void direction_tracker::load_data(std::string filePath)
 {
     std::ifstream file;
     file.open(filePath);
@@ -15,13 +17,14 @@ void direction_tracker::loadData(std::string filePath)
             std::istringstream ss(std::move(line));
             for (std::string value; std::getline(ss, value, ',');)
             {
-                std::string name = std::move(value);
-                danger_zone[name].arcStart = std::atof(std::move(value).c_str());
-                danger_zone[name].arcEnd = std::atof(std::move(value).c_str());
+                this->location_names.push_back(std::move(value));
+                this->arc_start.push_back(std::atof(std::move(value).c_str()));
+                this->arc_end.push_back(std::atof(std::move(value).c_str()));
 
-                std::cout << "Location: " << name <<
-                    ", Arc Start: " << danger_zone[name].arcStart <<
-                    ", Arc End: " << danger_zone[name].arcEnd << "\n";
+                int index = this->location_names.size() - 1;
+                std::cout << "Location: "    << this->location_names[index] <<
+                             ", Arc Start: " << this->arc_start[index]      <<
+                             ", Arc End: "   << this->arc_end[index]        << "\n";
             }
         }
     }
@@ -30,12 +33,49 @@ void direction_tracker::loadData(std::string filePath)
     file.close();
 }
 
-arc direction_tracker::get_arc(std::string name)
+std::vector<std::string> direction_tracker::get_location_names(float angle)
 {
-    return danger_zone[name];
+    std::vector<std::string> affectedLocs;
+    std::vector<int> arc_start_indices;
+    std::vector<int> arc_end_indices;
+
+    for (int i = 0; i < this->arc_start.size(); i++)
+    {
+        if (this->arc_start[i] <= angle)
+            arc_start_indices.push_back(i);
+    }
+    
+    for (int arcIndex : arc_start_indices)
+    {
+        if (this->arc_end[arcIndex] >= angle)
+            arc_end_indices.push_back(arcIndex);
+    }
+
+    for (int locIndex : arc_end_indices)
+        affectedLocs.push_back(this->location_names[locIndex]);
+
+    return affectedLocs;
+}
+
+void direction_tracker::set_wind_direction(float angle)
+{
+    this->wind_direction = angle;
 }
 
 void direction_tracker::show_gui()
 {
+    ImGui::Begin("Direction Tracker");
 
+    std::vector<std::string> affectedLocs = getLocationNames(this->wind_direction);
+    std::string affectedText;
+
+    for (int i = 0; i < affectedLocs.size(); i++)
+    {
+        affectedText += affectedLocs[i];
+        if (i < affectedLocs.size() - 1)
+            affectedText += ", ";
+    }
+
+    ImGui::Text(affectedText.c_str());
+    ImGui::End();
 }
--- code_plume/scenes/sources/smoke/direction_tracker.hpp
+++ code_plume/scenes/sources/smoke/direction_tracker.hpp
@@ -2,16 +2,22 @@
 
 #include <unordered_map>
 #include <string>
-
-struct arc { float arcStart, arcEnd; };
+#include <vector>
 
 class direction_tracker
 {
 private:
-	std::unordered_map<std::string, arc> danger_zone;
+	std::vector<float> arc_start;
+	std::vector<float> arc_end;
+	std::vector<std::string> location_names;
+
+	float wind_direction = 0.0f;
+
+	std::vector<std::string> get_location_names(float angle);
 
 public:
-	void loadData(std::string filePath);
-    arc get_arc(std::string name);
+
+	void set_wind_direction(float angle);
+	void load_data(std::string filePath);
     void show_gui();
 };
