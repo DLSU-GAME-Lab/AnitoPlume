b576f50b1e0dc517bde6e085e4a6e8c9feac316c
AZR437 <106365725+AZR437@users.noreply.github.com>
2025-05-29 08:59:09 +0800

    Tool Tip Fix

--- code_plume/out/build/scenes/sources/smoke/direction_tracker/direction_tracker.cpp
+++ code_plume/out/build/scenes/sources/smoke/direction_tracker/direction_tracker.cpp
@@ -32,44 +32,46 @@ void direction_tracker::load_data(std::string filePath)
 
 std::vector<std::string> direction_tracker::get_location_names(float angle)
 {
-    if (angle > 180) angle -= 360;
-
     std::vector<std::string> affected_locs;
-    std::vector<int> arc_start_indices;
-    std::vector<int> arc_end_indices;
+    float low_angle = angle - cone_radius;
+    float hi_angle = angle + cone_radius;
 
     for (int i = 0; i < this->arc_start.size(); i++)
     {
         float arc_start = this->arc_start[i];
+        float arc_end = this->arc_end[i];
 
-        if (arc_start > 180) arc_start -= 360;
-        if (arc_start <= angle) arc_start_indices.push_back(i);
+        if (arc_start > arc_end)
+        {
+            if (angle <= 180) arc_start -=360;
+            else if (angle > 180) arc_end += 360;
+        }
+
+        if ((arc_start <= low_angle || arc_start <= hi_angle) &&
+            (arc_end >= low_angle || arc_end >= hi_angle))
+            affected_locs.push_back(this->location_names[i]);
     }
-    
-    for (int arcIndex : arc_start_indices)
-    {
-        float arc_start = this->arc_start[arcIndex];
-        float arc_end = this->arc_end[arcIndex];
-
-        if (arc_start > 180) arc_start -= 360;
-        if (arc_end > 180 && arc_end - 360 > arc_start) arc_end -= 360;
-
-        if (arc_end >= angle) arc_end_indices.push_back(arcIndex);
-    }
-
-    for (int locIndex : arc_end_indices)
-        affected_locs.push_back(this->location_names[locIndex]);
 
     return affected_locs;
+}
+
+vcl::vec3 direction_tracker::angle_to_vector(float angle) const
+{
+    double radians = (angle * pi) / 180;
+    return vcl::vec3(cos(radians), sin(radians), 0);
+}
+
+float direction_tracker::vector_to_angle(vcl::vec3 vector) const
+{
+    double radians = atan2(wind_vector.y, wind_vector.x);
+    if (radians < 0) radians += (2 * pi);
+    return radians * (180.0 / pi);
 }
 
 void direction_tracker::set_wind_direction(vcl::vec3 wind_vector)
 {
     this->wind_vector = wind_vector;
-    const double pi = 3.14159;
-    double radians = atan2(wind_vector.y, wind_vector.x);
-    if (radians < 0) radians +=  (2 * pi);
-    this->wind_angle = radians * (180.0 / pi);
+    this->wind_angle = vector_to_angle(wind_vector);
 }
 
 void direction_tracker::show_gui()
@@ -82,12 +84,21 @@ void direction_tracker::show_gui()
     ImGui::Image((ImTextureID)danger_zone_image, { image_size, image_size });
     std::vector<std::string> affected_locs = get_location_names(this->wind_angle);
 
+
     ImDrawList* draw_list = ImGui::GetWindowDrawList();
     ImVec2 win_pos = ImGui::GetWindowPos();
-    win_pos.x += 6; win_pos.y += 26;
+    win_pos.x += 8; win_pos.y += 27;
     ImVec2 start = ImVec2(win_pos.x + half_size, win_pos.y + half_size);
-    ImVec2 end = ImVec2(start.x + (wind_vector.x * line_len), start.y + (-wind_vector.y * line_len));
-    draw_list->AddLine(start, end, IM_COL32(240, 0, 20, 255), 4.0f);
+
+    float angle = vector_to_angle(wind_vector);
+    vcl::vec3 wind1 = angle_to_vector(angle + cone_radius);
+    vcl::vec3 wind2 = angle_to_vector(angle - cone_radius);
+
+    ImVec2 end1 = ImVec2(start.x + (wind1.x * line_len), start.y + (-wind1.y * line_len));
+    ImVec2 end2 = ImVec2(start.x + (wind2.x * line_len), start.y + (-wind2.y * line_len));
+    draw_list->AddLine(start, end1, IM_COL32(240, 0, 40, 255), 2.0f);
+    draw_list->AddLine(start, end2, IM_COL32(240, 0, 20, 255), 2.0f);
+    draw_list->AddTriangleFilled(start, end1, end2, IM_COL32(240, 0, 40, 100));
 
     ImGui::SameLine();
     ImGui::BeginChild("Affected Areas", {200.0f, image_size}, true);
--- code_plume/out/build/x64-Debug/scenes/sources/smoke/direction_tracker/direction_tracker.hpp
+++ code_plume/out/build/x64-Debug/scenes/sources/smoke/direction_tracker/direction_tracker.hpp
@@ -14,10 +14,14 @@ private:
 	std::vector<float> arc_end;
 	std::vector<std::string> location_names;
 
+	const double pi = 3.14159;
+	float cone_radius = 15.0f;
 	float wind_angle = 0.0f;
 	vcl::vec3 wind_vector = { 1.0f, 0.0f, 0.0f };
 
 	std::vector<std::string> get_location_names(float angle);
+	vcl::vec3 angle_to_vector(float angle) const;
+	float vector_to_angle(vcl::vec3 vector) const;
 
 public:
 	void set_wind_direction(vcl::vec3 wind_vector);
